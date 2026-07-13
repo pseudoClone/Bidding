df76225cd7c9590e1dabd8a8941f01d527f2c363
pseudoclone <prince.077bei027@acem.edu.np>
2026-07-13 20:41:48 +0545

    Move session generator to engine to fix circular dependency error in DI

--- src/engine.py
+++ src/engine.py
@@ -1,4 +1,4 @@
-from sqlmodel import create_engine, SQLModel
+from sqlmodel import create_engine, SQLModel, Session
 from dotenv import load_dotenv
 import os
 
@@ -9,6 +9,11 @@ engine = create_engine(DATABASE_URL, echo=True)
 
 def init_db():
     SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
 
 
 init_db()
