ba2bd5629edbf2563fbc849d49ff4c951b801184
pseudoclone <prince.077bei027@acem.edu.np>
2026-07-13 20:24:11 +0545

    Intercept with JWT for this function to any thing that requires this as dependency

--- src/security.py
+++ src/security.py
@@ -3,6 +3,12 @@ from pwdlib import PasswordHash
 from dotenv import load_dotenv
 import os
 import jwt
+from jwt import PyJWTError
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from sqlmodel import Session
+from .main import get_session
+from .model import User
 
 load_dotenv()
 
@@ -31,3 +37,28 @@ def createAccessToken(data: dict, expiresDelta: timedelta | None = None):
     )
     toEncode.update({"exp": expire})
     return jwt.encode(toEncode, SECRET_KEY, algorithm=ALGORITHM)
+
+
+oauth2Scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+def getCurrentUser(
+    token: str = Depends(oauth2Scheme), session: Session = Depends(get_session)
+) -> User:
+    credentialsException = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="You are not authorized to do this",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        userID = payload.get("sub")
+        if userID is None:
+            raise credentialsException
+    except PyJWTError:
+        raise credentialsException
+
+    user = session.get(User, userID)
+    if user is None:
+        raise credentialsException
+    return user
