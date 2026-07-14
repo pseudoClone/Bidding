53f6b273401406a6fe01dc76013d62fcea1794f6
pseudoclone <prince.077bei027@acem.edu.np>
2026-07-14 15:59:29 +0545

    Add websocket manager to handle live connections in memory

---
+++ src/websocketManager.py
@@ -0,0 +1,51 @@
+from uuid import UUID
+from fastapi import WebSocket
+import logging
+
+"""
+Since, HTTP is stateless, we have no way to manage connections to our API by 
+clients.
+We could use DB for this but frequent read and writes to PostgreSQL by throwaway
+connections does not make sense. So, instead we save it in memory, this will 
+take up some memory but make our work fast. Maybe in future, we use Redis or 
+something like that.
+"""
+
+logger = logging.getLogger(__name__)
+
+
+class ConnnectionManager:
+    def __init__(self) -> None:
+        self.activeConnections: dict[str, list[WebSocket]] = {}
+
+    async def connect(self, webSocket: WebSocket, auctionID: UUID):
+        await webSocket.accept()
+        auctionKey = str(auctionID)
+        if auctionKey not in self.activeConnections:
+            self.activeConnections[auctionKey] = []
+            """
+            Ig this is similar to struct{}{} in go. Because we could have saved
+            this is a set data structure but that would consume extra bytes.
+            """
+        self.activeConnections[auctionKey].append(webSocket)
+
+    def disconnect(self, webSocket: WebSocket, auctionID: UUID):
+        auctionKey = str(auctionID)
+        if auctionKey in self.activeConnections:
+            self.activeConnections[auctionKey].remove(webSocket)
+            if not self.activeConnections[auctionKey]:
+                del self.activeConnections[auctionKey]
+
+    async def broadcastToAuction(self, auctionID: UUID, message: dict):
+        auctionKey = str(auctionID)
+        if auctionKey in self.activeConnections:
+            for connection in self.activeConnections[auctionKey]:
+                try:
+                    await connection.send_json(message)
+                except Exception as e:
+                    logger.warning(
+                        f"Failed to send message in auction {auctionKey}:\t{e}"
+                    )
+
+
+manager = ConnnectionManager()
